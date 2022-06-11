da20dcb8f69d0830e061f0f56d69b43155ff995c
Filipe Garrote <filipe.garrote@gmail.com>
2022-06-11 13:52:15 -0300

    fix JS v.1 - Border + Check icon

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
-function selectFood (classOption){
-  const icone = document.querySelector(".icon");
-  const selecao = document.querySelector(".itens1 > .option-border");
-    if (selecao !== null && icone !== null){
-      selecao.classList.remove("option-border");
-      icone.classList.remove("check");
-    }
-  classOption.classList.add("option-border");
-  icone.classList.add("check");
-}
+function selectFood(classOption, classItens){
+  const element = document.querySelector(classItens);
+  const icone = classOption.querySelector(".icon");
+  const hiddenIcon = element.querySelector(".check");
+  const food = element.querySelector(".option-border");
+  
+  if(food !== null){
+    food.classList.remove("option-border");
+    hiddenIcon.classList.remove("check");
+  }
 
-function selectDrink (classOption){
-  const selecao = document.querySelector(".itens2 > .option-border")
-    if (selecao !== null){
-      selecao.classList.remove("option-border");
-    }
   classOption.classList.add("option-border");
-}
-
-function selectDessert (classOption){
-  const selecao = document.querySelector(".itens3 > .option-border")
-    if (selecao !== null){
-      selecao.classList.remove("option-border");
-    }
-  classOption.classList.add("option-border");
+  icone.classList.add("check")
 }
 
 
 
-/*function selectProduct(){
-  const element = document.querySelector(".option");
-  element.classList.toggle("option-border");
-  const ion = document.querySelector("ion-icon");
-  ion.classList.toggle("check");
+
+
+
+/*function selectFood(classOption){
+  const icone = classOption.querySelector(".icon");
+  const hiddenIcon = document.querySelector(".check");
+  const food = document.querySelector(".itens1 > .option-border");
+  
+  if(food !== null){
+    food.classList.remove("option-border");
+    hiddenIcon.classList.remove("check");
+  }
+
+  classOption.classList.add("option-border");
+  icone.classList.add("check")
+}
+
+function selectDrink(classOption){
+  const icone = classOption.querySelector(".icon");
+  const hiddenIcon = document.querySelector(".check");
+  const food = document.querySelector(".itens2 > .option-border");
+  
+  if(food !== null){
+    food.classList.remove("option-border");
+    hiddenIcon.classList.remove("check");
+  }
+
+  classOption.classList.add("option-border");
+  icone.classList.add("check")
+}
+
+function selectDessert(classOption){
+  const icone = classOption.querySelector(".icon");
+  const hiddenIcon = document.querySelector(".check");
+  const food = document.querySelector(".itens3 > .option-border");
+  
+  if(food !== null){
+    food.classList.remove("option-border");
+    hiddenIcon.classList.remove("check");
+  }
+
+  classOption.classList.add("option-border");
+  icone.classList.add("check")
+}/*
+
+/*
+
+function finalizarCompra(){
+  let food = document.querySelector(".itens1 > .option-border");
+  let drink = document.querySelector(".itens2 > .option-border");
+  let dessert = document.querySelector(".itens3 > .option-border");
+  if(food !== null && drink !== null && dessert !== null){
+    const hidden = document.querySelector(".footer > button");
+    hidden.classList.remove("buttonInitial");
+    hidden.classList.add("buttonFinal");
+    hidden.innerHTML = "Finalizar Pedido"
+  }
 }*/
 
