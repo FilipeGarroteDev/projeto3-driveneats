7368cc775c7165ac7fb421bc52b185ead35d06fa
Filipe Garrote <filipe.garrote@gmail.com>
2022-06-11 01:37:18 -0300

    JS v.2 - Seleção dos 3 itens

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-function selectProduct(){
+function selectFood (classOption){
+  const icone = document.querySelector(".icon");
+  const selecao = document.querySelector(".itens1 > .option-border");
+    if (selecao !== null && icone !== null){
+      selecao.classList.remove("option-border");
+      icone.classList.remove("check");
+    }
+  classOption.classList.add("option-border");
+  icone.classList.add("check");
+}
+
+function selectDrink (classOption){
+  const selecao = document.querySelector(".itens2 > .option-border")
+    if (selecao !== null){
+      selecao.classList.remove("option-border");
+    }
+  classOption.classList.add("option-border");
+}
+
+function selectDessert (classOption){
+  const selecao = document.querySelector(".itens3 > .option-border")
+    if (selecao !== null){
+      selecao.classList.remove("option-border");
+    }
+  classOption.classList.add("option-border");
+}
+
+
+
+/*function selectProduct(){
   const element = document.querySelector(".option");
   element.classList.toggle("option-border");
   const ion = document.querySelector("ion-icon");
   ion.classList.toggle("check");
-}
+}*/
 
